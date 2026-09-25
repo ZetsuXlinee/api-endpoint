9cfcf964a282a1d3ba83eacb1963ad84025f0d2c
ZetsuXlinee <dzikozik@gmail.com>
2026-09-26 02:43:20 +0700

    Serve HTML file in API endpoint

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-module.exports = require('../server.js');
+const fs = require('fs');
+const path = require('path');
+module.exports = (req, res) => {
+  const filePath = path.join(__dirname, '../html/index.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.send(html);
+}
