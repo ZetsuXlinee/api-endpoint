c6640df1d9dfdab86133ab9cd0c058e88ff0eb0e
ZetsuXlinee <dzikozik@gmail.com>
2026-09-26 02:45:41 +0700

    Implement error handling in index.js
    Add error handling for HTML file reading

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = (req, res) => {
-  const filePath = path.join(__dirname, '../html/index.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html');
-  res.send(html);
+  try {
+    const filePath = path.join(__dirname, '../html/index.html');
+    const html = fs.readFileSync(filePath, 'utf8');
+    res.setHeader('Content-Type', 'text/html');
+    return res.send(html);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 }
